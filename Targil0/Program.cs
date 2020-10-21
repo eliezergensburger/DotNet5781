88b83bf98eab3cd23109c35dd26f96c724b924d7
eliezer gensburger <eliezer.jct@gmail.com>
2020-10-21 16:59:16 +0300

    kuku say hello

---
+++ Targil0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloKuku();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void HelloKuku()
+        {
+            Console.WriteLine("kuku strike again");
+        }
+    }
+}
