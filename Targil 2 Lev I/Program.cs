ee1e31dc57a324aec5859e43d7a770766e339bd8
Eliezer Gensburger <eliezer.jct@gmail.com>
2020-10-28 18:11:24 +0200

    beware of Zoom bee

--- Targil 2 Lev I/Program.cs
+++ Targil 2 Lev I/Program.cs
@@ -10,6 +10,19 @@ namespace Targil_2_Lev_I
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hachness mispar (yanoo):");
+            string kelet = Console.ReadLine();
+            bool success = Int32.TryParse(kelet, out _);
+            if(success)
+            {
+                Console.WriteLine("eize yofi");
+            }
+            else
+            {
+                Console.WriteLine("Tooo BBBBBBAAAAADDD !!!!!!");
+            }
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
