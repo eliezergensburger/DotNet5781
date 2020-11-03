8711a03e8cc089cff1df02c53cae8de2fb8a171b
Eliezer Gensburger <eliezer.jct@gmail.com>
2020-11-03 18:32:57 +0200

    behatzlacha

--- Targil 2/Targil02_Tal_DotNetLab/BusLine.cs
+++ Targil 2/Targil02_Tal_DotNetLab/BusLine.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 
 namespace Targil02_Tal_DotNetLab
 {
-    public class BusLine
+    public class BusLine 
     {
         private List<BusStation> busstations = new List<BusStation>();
 
+        public BusLine()
+        {
+
+        }
         public List<BusStation> BusStations
         {
             get { return busstations; }
@@ -15,9 +20,28 @@ namespace Targil02_Tal_DotNetLab
         /// Line number
         /// </summary>
         public int Number { get; set; }
-        public BusStation FirstStation { get; set; }
-        public BusStation LastStation { get; set; }
+        public BusStation FirstStation {get; private set;}
+        public BusStation LastStation { get; private set; }
         public Zone Zone { get; set; }
+
+        public void AddLast(BusStation busStation)
+        {
+            busstations.Add(busStation);
+            LastStation = busstations[busstations.Count -1];
+        }
+        public void AddFirst(BusStation busStation)
+        {
+            busstations.Insert(0, busStation);
+            FirstStation = busstations[0];
+         }
+        public void Add(int index, BusStation busStation)
+        {
+            busstations.Insert(index, busStation);
+            if(index == 0)
+            {
+                FirstStation = busstations[0];
+            }
+        }
 
     }
 }
