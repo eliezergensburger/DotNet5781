1f98b16cc3b51ecf2b55f433d7e63cb477cdbc08
Eliezer Gensburger <eliezer.jct@gmail.com>
2020-11-08 22:30:53 +0200

    message

--- Targil 2/Targil02_Tal_DotNetLab/BusLine.cs
+++ Targil 2/Targil02_Tal_DotNetLab/BusLine.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 
 namespace Targil02_Tal_DotNetLab
 {
-    public class BusLine
+    public class BusLine:IComparable<BusLine>
     {
         private List<BusStation> busstations = new List<BusStation>();
         public List<BusStation> BusStations
@@ -60,5 +58,28 @@ namespace Targil02_Tal_DotNetLab
                 }
              }
          }
+        public double timeBetween(BusStation one, BusStation two)
+        {
+            return one.TravelTime.Subtract(two.TravelTime).TotalMinutes;
+        }
+
+        public int CompareTo(BusLine other)
+        {
+            double mytotal = totalTime();
+            double othertotal = other.totalTime();
+  
+            return mytotal.CompareTo(othertotal);
+        }
+
+        private double totalTime()
+        {
+            double total = 0;
+            for (int i = 0; i < busstations.Count - 1; i++)
+            {
+                total += timeBetween(busstations[i], busstations[i + 1]);
+            }
+
+            return total;
+        }
     }
 }
