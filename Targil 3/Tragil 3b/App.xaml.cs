b1ee1210b8d2d89e91cfa04baee2c9196380a293
Eliezer Gensburger <eliezer.jct@gmail.com>
2020-11-16 00:08:57 +0200

    kuku

---
+++ Targil 3/Tragil 3b/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tragil_3b
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
