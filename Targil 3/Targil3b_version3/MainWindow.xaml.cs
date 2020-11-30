2069c5099fe64f169e29ef3ffd0a6e3f49aed791
Eliezer Gensburger <eliezer.jct@gmail.com>
2020-11-30 09:18:22 +0200

    yesh

--- Targil 3/Targil3b_version3/MainWindow.xaml.cs
+++ Targil 3/Targil3b_version3/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,28 +26,100 @@ namespace Targil3b_version3
         public MainWindow()
         {
             InitializeComponent();
-            List<User> items = new List<User>();
+            this.Closing += MainWindow_Closing;
+
+            // List<User> items = new List<User>();
+            ObservableCollection<User> items = new ObservableCollection<User>();
             items.Add(new User() { Name = "John Doe", Age = 42 });
             items.Add(new User() { Name = "Jane Doe", Age = 39 });
             items.Add(new User() { Name = "Sammy Doe", Age = 13 });
             items.Add(new User() { Name = "Donna Doe", Age = 13 });
             lvUsers.ItemsSource = items;
+        }
 
-         }
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void Tidluk_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Button btn = e.Result as Button;
+            btn.IsEnabled = true;
+            btn.Background = Brushes.Green;
+   //         throw new NotImplementedException();
+        }
+
+        private void Tidluk_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<Object> lst = (List<object>)e.Argument;
+
+            int value = (int)lst[0];    //3000
+            Thread.Sleep(value);
+
+            //idkun delek acharei hamiluy
+            User currentUser = (User)lst[1];
+            currentUser.Age += 10;
+
+            e.Result = lst[2];          //btn
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var fxElt = sender as FrameworkElement;
-            User lineData = fxElt.DataContext as User;
-            MessageBox.Show(lineData.Name);
+            Button btn = sender as Button;
+            User currentuser = btn.DataContext as User;
+
+            btn.IsEnabled = false;
+
+            tidluk(currentuser, 3000, btn);
         }
-           
 
+        private void tidluk(User lineData, int time, Button btn)
+        {
+            List<Object> lst = new List<object> { time, lineData, btn };
+
+            BackgroundWorker tidluk = new BackgroundWorker();
+            tidluk.DoWork += Tidluk_DoWork;
+            tidluk.RunWorkerCompleted += Tidluk_RunWorkerCompleted;
+
+            tidluk.RunWorkerAsync(lst);
+        } 
     }
-    public class User
-    {
-        public string Name { get; set; }
 
-        public int Age { get; set; }
+    public class User : INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+
+        public string Name 
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        protected void OnPropertyChanged(String propertyName)
+        {
+            //if (PropertyChanged != null)
+            //{
+            //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            //}
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int Age 
+        {
+            get => age;
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
